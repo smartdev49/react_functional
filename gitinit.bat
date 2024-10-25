eb62e4b34fca62141cccd86f6811cf3370965af0
unknown <curtisjowen@gmail.com>
2024-10-25 02:01:59 -0400

    use context

--- gitinit.bat
+++ gitinit.bat
@@ -1,7 +1,5 @@
-@REM git init
-@REM git add .
-@REM git config user.email "curtisjowen@gmail.com"
-@REM git remote add origin git@github-curtis:smartdev49/react_functional.git
-
-@REM git commit -m ""
-@REM git push 
+git init
+git add .
+git config user.email "curtisjowen@gmail.com"
+git remote add origin git@github-curtis:smartdev49/react_functional.git
+git push --set-upstream origin master
